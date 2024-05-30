a93e62ba30f9202d52499ca49119e151498517b6
Marcus Bowyer <marcus@euarctos.com>
2024-05-30 16:09:30 -0700

    rename BitDriftProxy to BdProxy to shorten package paths (nuget restore was breaking because of the path size limit)

---
+++ src/BitDrift.iOS.Binding/native/BdProxy/BdProxy.h
@@ -0,0 +1,17 @@
+//
+//  BdProxy.h
+//  BdProxy
+//
+//  Created by Marcus Bowyer on 4/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for BdProxy.
+// FOUNDATION_EXPORT double BdProxyVersionNumber;
+
+//! Project version string for BdProxy.
+// FOUNDATION_EXPORT const unsigned char BdProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BdProxy/PublicHeader.h>
