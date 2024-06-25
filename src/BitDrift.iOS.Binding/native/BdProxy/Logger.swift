b0fa88ace3650c25b3ef7af557e2951e9d29a7ce
Marcus Bowyer <marcus@euarctos.com>
2024-06-25 09:55:57 -0700

    chore: Refactor Logger.swift to handle unexpected errors during configuration and logging

--- src/BitDrift.iOS.Binding/native/BdProxy/Logger.swift
+++ src/BitDrift.iOS.Binding/native/BdProxy/Logger.swift
@@ -5,71 +5,106 @@ public class Logger: NSObject {
 
     @objc
     public static func configure(key: String, url: String, strategy: String) {
-        switch strategy {
-        case "fixed":
-            Capture.Logger.configure(
-                withAPIKey: key,
-                sessionStrategy: SessionStrategy.fixed(),
-                apiURL: URL(string: url)!)
-        case "activity-based":
-            Capture.Logger.configure(
-                withAPIKey: key,
-                sessionStrategy: SessionStrategy.activityBased(),
-                apiURL: URL(string: url)!)
-        default:
-            return
+        do
+        {
+            switch strategy {
+            case "fixed":
+                Capture.Logger.configure(
+                    withAPIKey: key,
+                    sessionStrategy: SessionStrategy.fixed(),
+                    apiURL: URL(string: url)!)
+            case "activity-based":
+                Capture.Logger.configure(
+                    withAPIKey: key,
+                    sessionStrategy: SessionStrategy.activityBased(),
+                    apiURL: URL(string: url)!)
+            default:
+                return
+            }
+        } catch {
+           print("Unexpected error: \(error).")
         }
     }
 
     @objc
     public static func log(_ level: Double, message: String, fields: [String: String]) {
-        switch level {
-        case 0.0:
-            Capture.Logger.logTrace(message, fields: fields)
-        case 1.0:
-            Capture.Logger.logDebug(message, fields: fields)
-        case 2.0:
-            Capture.Logger.logInfo(message, fields: fields)
-        case 3.0:
-            Capture.Logger.logWarning(message, fields: fields)
-        case 4.0:
-            Capture.Logger.logError(message, fields: fields)
-        default:
-            return
+        do
+        {
+            switch level {
+            case 0.0:
+                Capture.Logger.logTrace(message, fields: fields)
+            case 1.0:
+                Capture.Logger.logDebug(message, fields: fields)
+            case 2.0:
+                Capture.Logger.logInfo(message, fields: fields)
+            case 3.0:
+                Capture.Logger.logWarning(message, fields: fields)
+            case 4.0:
+                Capture.Logger.logError(message, fields: fields)
+            default:
+                return
+            }
+        } catch {
+           print("Unexpected error: \(error).")
         }
     }
     
     @objc
     public static func sessionId() -> String
     {
-        let result = Capture.Logger.sessionID;
-        return result!;
+        do
+        {
+            let result = Capture.Logger.sessionID;
+            return result!;
+        } catch {
+           print("Unexpected error: \(error).")
+           return ""
+        }
     }
     
     @objc
     public static func sessionUrl() -> String
     {
-        let result = Capture.Logger.sessionURL;
-        return result!;
+        do
+        {
+            let result = Capture.Logger.sessionURL;
+            return result!;
+        } catch {
+           print("Unexpected error: \(error).")
+           return ""
+        }
     }
     
     @objc
     public static func startNewSession()
     {
-        Capture.Logger.startNewSession();
+        do
+        {
+            Capture.Logger.startNewSession();
+        } catch {
+           print("Unexpected error: \(error).")
+        }
     }
     
     @objc
     public static func addField(key: String, value: String)
     {
-        Capture.Logger.addField(withKey: key, value: value);
+        do
+        {
+            Capture.Logger.addField(withKey: key, value: value);
+        } catch {
+           print("Unexpected error: \(error).")
+        }
     }
     
     @objc
     public static func removeField(key: String)
     {
-        Capture.Logger.removeField(withKey: key)
+        do
+        {
+            Capture.Logger.removeField(withKey: key)
+        } catch {
+           print("Unexpected error: \(error).")
+        }
     }
-    
-    
 }
